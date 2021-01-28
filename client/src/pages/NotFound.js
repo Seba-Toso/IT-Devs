2e480ff564996dd6c497de9cf8ff43f0706aed0c
Cristian Machuca <cmachuca32@gmail.com>
2021-01-28 02:12:30 +0200

    test

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -5,7 +5,12 @@ import './NotFound.css';
 const NotFound = () =>{
     return(
         <div className="NotFound">
-            <img src="https://image.freepik.com/vector-gratis/pagina-error-404-no-encontrada_41910-364.jpg" alt='' />
+            <div className='sloganContainer'>
+                  <h1 className='sloganText One'>Error</h1>
+                  <h1 className='sloganText Two'>404</h1>
+                  <h1 className='sloganText Three'>Sorry</h1>
+          </div>
+            <img src="https://www.seoptimer.com/es/blog/wp-content/uploads/2015/07/Destacada.jpg" alt='' />
         </div>
     )
 };
