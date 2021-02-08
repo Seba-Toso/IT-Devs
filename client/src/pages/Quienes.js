f52cf215fb2c4d1363f62f17744f6768362dcd45
Cristian Machuca <cmachuca32@gmail.com>
2021-02-08 22:09:08 +0200

    Page Quienes Done

--- client/src/pages/Quienes.js
+++ client/src/pages/Quienes.js
@@ -17,10 +17,25 @@ const QuienesSomos = () =>{
                         <h1 className='sloganText Three'>TEAM</h1>
                     </div>
             </div>
+            <div>
+            <div className='logoContainer'>
+                    <img src='assets/fullLogo.png' height={'400px'} alt='Logo'/>
+            
+            <p>IT-Devs es un Proyecto emprendedor diseñado y dirigido por dos</p>
+            <p>Developers en entorno de Stack MERN. El objetivo principal del</p>
+            <p>emprendimiento es generar Soluciones de Desarrollo Web para</p>
+            <p>quienes quieren ingresar sus Negocios en el mundo del Comercio</p>
+            <p>Electrónico y así llegar mejor a potenciales Clientes y generar</p>
+            <p>mayores ingresos, deja que la Tecnología lo haga por Vos.</p>
+            </div>
+            </div>
             <div className="Container projectSlider section">
                 <div className="Card">         
                     <img src="http://imgfz.com/i/uS40TwE.jpeg" alt="" />
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                    <p>Desarrollador FullStack MERN.
+                    Estudiante de Desarrollo de Software.
+                    Emprendedor Web.</p>
+                    <p></p>
                     <h2>Cristian Machuca</h2>
                     <h4>Developer</h4>
                     <div className="Links">
