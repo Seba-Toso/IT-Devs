89f8c7dbfe53cccf871f11b43c067cec24de1e97
Cristian Machuca <cmachuca32@gmail.com>
2021-02-08 19:26:28 +0200

    Personal Cards added

--- client/src/pages/Quienes.js
+++ client/src/pages/Quienes.js
@@ -1,64 +1,48 @@
 import React from 'react';
+import * as ReactIcons from 'react-icons/di'
+import * as IconName from "react-icons/fa";
 import './Quienes.css';
 
 
 const QuienesSomos = () =>{
 
 
-    const developers = [
-        {
-            id: 1,
-            name: 'CRISTIAN MACHUCA',
-            avatar: 'http://imgfz.com/i/uS40TwE.jpeg',
-            description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.  '
-        },
-        {
-            id: 2,
-            name: 'SEBASTIÁN TOSO',
-            avatar: 'https://avatars.githubusercontent.com/u/66390886?s=460&u=cd0fd00c67e5d0a2c83dcbe99574a6f9c50bf3ee&v=4',
-            description: 'Mamáááááááá! Sacá la mano de ahí car$%#, cortaste toda la loz!!'
-        },
-    ]
-
     return(
-        <div className='section profiles'>
-            <div className='section'>
-                <div className='sloganContainer'>
-                        <h1 className='sloganText One'>SOMETHING</h1>
-                        <h1 className='sloganText Two'>ABOUT</h1>
-                        <h1 className='sloganText Three'>US.</h1>
-                </div>
+        <div>
+            <div className='section intro'>
+                    
+                    <div className='sloganContainer'>
+                        <h1 className='sloganText One'>MEET</h1>
+                        <h1 className='sloganText Two'>THE</h1>
+                        <h1 className='sloganText Three'>TEAM</h1>
+                    </div>
             </div>
-
-            <div className='section'>
-                <p className='aboutText'> Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious. Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.Bla bla bla, bibidibabidibu. Supercalifragilisticoespialidoucious.
-                </p>
-            </div>
-
-            <div className='section'>  
-                <h1 className='profileTitle One'>MEET THE TEAM.</h1>
-                {
-                    developers.map( developer => {
-                        return (
-                            <div className='profileContainer' key={developer.id}>
-                                <div className="avatarContainer">
-                                    <img src={developer.avatar} id="avatar" alt=""/>
-                                </div>
-                                <div className='profileInformation'>
-                                    <h3 className='profileName'>{developer.name}</h3>
-                                    <p className='profileDescription'>{developer.description}</p>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </div>
+            <div className="Container">
+                <div className="Card">         
+                    <img src="http://imgfz.com/i/uS40TwE.jpeg" alt="" />
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                    <h2>Cristian Machuca</h2>
+                    <h4>Developer</h4>
+                    <div className="Links">
+                                <div id="git"><ReactIcons.DiGithubBadge /></div>  
+                                <div id="linkedin"><IconName.FaLinkedin /></div>
+                                <div id="youtube"><IconName.FaYoutube /></div>
+                    </div>
+                </div>  
+                <div className="Card">         
+                <img src="https://avatars.githubusercontent.com/u/66390886?s=460&u=cd0fd00c67e5d0a2c83dcbe99574a6f9c50bf3ee&v=4" alt="" />
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting </p>
+                    <h2>Sebastian Toso</h2>
+                    <h4>Developer</h4>
+                    <div className="Links">
+                                <div id="git"><ReactIcons.DiGithubBadge /></div>  
+                                <div id="linkedin"><IconName.FaLinkedin /></div>
+                                <div id="youtube"><IconName.FaYoutube /></div>
+                    </div>
+                </div>       
+            </div> 
+     </div>
+    
     )
 
 };
