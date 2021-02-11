1867a8b20e1aead772434bad2b197bf9065fe711
Cristian Machuca <cmachuca32@gmail.com>
2021-02-11 19:25:16 +0200

    Page Portfolio Done

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -3,87 +3,128 @@ import './Porfolio.css';
 
 const Portfolio = () =>{
   
-  const products= [
-    {
-      id: '1',
-      image: 'http://imgfz.com/i/oVrt6KT.jpeg',
-      title: 'Blog Python',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '2',
-      image: 'http://imgfz.com/i/o5IHu0b.jpeg',
-      title: 'E-commerce',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '3',
-      image: 'http://imgfz.com/i/WHgpPY8.jpeg',
-      title: 'API REST NODE JS',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '4',
-      image: 'http://imgfz.com/i/ojQLO6e.jpeg',
-      title: 'E-commerce',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '5',
-      image: 'http://imgfz.com/i/o5IHu0b.jpeg',
-      title: 'E-commerce',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '6',
-      image: 'http://imgfz.com/i/WHgpPY8.jpeg',
-      title: 'API REST NODE JS',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    },
-    {
-      id: '7',
-      image: 'http://imgfz.com/i/ojQLO6e.jpeg',
-      title: 'E-commerce',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-    }  
-  ]
+
 
   return(
-      <div className='Products-items'>
-        <div className='section'>
-          <div className='sloganContainer'>
-                  <h1 className='sloganText One'>WHAT</h1>
-                  <h1 className='sloganText Two'>WE</h1>
-                  <h1 className='sloganText Three'>DID.</h1>
-          </div>
-        </div>
-        <div className='section portfolioGrid'>
-          {
-            products.map( project => { 
-              return (
-                <div className='portfolioItem' key={project.id} style={{backgroundImage: `url(${project.image})`}}> 
-                  <div className='projectInfo'>{project.title}</div>
+        <div>
+            <div className='section intro'>
+                <div className='logoContainer'>
+                    <img src='assets/fullLogo.png' height={'400px'} alt='Logo'/>
                 </div>
-              )
-            })
-          }
-        </div>
+                <div className='sloganContainer'>
+                    <h1 className='sloganText One'>CONOCE</h1>
+                    <h1 className='sloganText Two'>NUESTROS</h1>
+                    <h1 className='sloganText Three'>PROYECTOS.</h1>
+                </div>
+            </div>
 
-        {/*
-        <hr/>
-          <ul>
-              <li><img src='http://imgfz.com/i/PmSDd1U.jpeg' id='Moda' /></li>
-              <h2>Tienda de Moda</h2><span>Registro de Usuarios.</span>
-              <li><img src='http://imgfz.com/i/o5IHu0b.jpeg' /></li>
-              <h2>E-commerce</h2><span>Tienda con carrito de compras.</span>
-              <li><img src='http://imgfz.com/i/VWsILRQ.jpeg' id='Vinos'/></li>
-              <h2>E-commerce de Vinos</h2><span>Registro de Usuarios.</span>
-              <li><img src='http://imgfz.com/i/ojQLO6e.jpeg'/></li>
-              <h2>E-commerce</h2><span>Tienda con carrito de compras.</span>
-          </ul>
-        <hr/>
-        */}
-      </div>
+
+
+              <div class="portfolio">
+                <div class="portfolio__item">
+                     <img src="http://imgfz.com/i/oVrt6KT.jpeg" alt="" class="portfolio__image"/>
+                  <div class="portfolio__desc">
+                    <h3 class="portfolio__title">Blog</h3>
+                    <a href="#portfolio-item-1" class="btn btn-primary" role="button">Más</a>
+                </div>
+                
+
+              </div>
+                <div class="portfolio__item" id="portfolio">
+                      <img src="http://imgfz.com/i/o5IHu0b.jpeg" alt="" class="portfolio__image"/>
+                  <div class="portfolio__desc">
+                    <h3 class="portfolio__title">E-commerce</h3>
+                    <a href="#portfolio-item-2"class="btn btn-primary">Más</a>
+                  </div>
+                </div>
+                    <div class="portfolio__item">
+                      <img src="http://imgfz.com/i/ojQLO6e.jpeg" alt="" class="portfolio__image"/>
+                      <div class="portfolio__desc">
+                        <h3 class="portfolio__title">E-commerce</h3>
+                        <a href="#portfolio-item-3"class="btn btn-primary">Más</a>
+                      </div>
+                    </div>
+                </div>
+
+                <div class="portfolio-lightbox" id="portfolio-item-1">
+                  <div class="portfolio-lightbox__content">
+                  <a href="#portfolio" class="close"></a> 
+                  <img class="portfolio-lightbox__image" src="http://imgfz.com/i/oVrt6KT.jpeg" alt=""/>
+                  <h3 class="portfolio__title">Blog</h3>
+                  <p class="portfolio__body">Sitio diseñado con Python y Flask en base de SQL.</p>
+                  </div>
+                </div> 
+
+                <div class="portfolio-lightbox" id="portfolio-item-2">
+                  <div class="portfolio-lightbox__content">
+                    <a href="#portfolio" class="close"></a>
+                    <img class="portfolio-lightbox__image" src="http://imgfz.com/i/o5IHu0b.jpeg" alt=""/>
+                    <h3 class="portfolio__title">E-commerce</h3>
+                    <p class="portfolio__body">Web diseñada en Front con Angular y Backend en Node - Express.</p>
+                  </div>
+                </div> 
+
+                <div class="portfolio-lightbox" id="portfolio-item-3">
+                  <div class="portfolio-lightbox__content">
+                  <a href="#portfolio" class="close"></a>
+                  <img class="portfolio-lightbox__image" src="http://imgfz.com/i/ojQLO6e.jpeg" alt=""/>
+                  <h3 class="portfolio__title">E-commerce</h3>
+                  <p class="portfolio__body">Web diseñada en Front con React y Backend en Node - Express.</p>
+                  </div>
+                </div> 
+                <div class="portfolio">
+                <div class="portfolio__item">
+                     <img src="http://imgfz.com/i/oVrt6KT.jpeg" alt="" class="portfolio__image"/>
+                  <div class="portfolio__desc">
+                    <h3 class="portfolio__title">Blog</h3>
+                    <a href="#portfolio-item-1" class="btn btn-primary" role="button">Más</a>
+                </div>
+                
+
+              </div>
+                <div class="portfolio__item" id="portfolio">
+                      <img src="http://imgfz.com/i/o5IHu0b.jpeg" alt="" class="portfolio__image"/>
+                  <div class="portfolio__desc">
+                    <h3 class="portfolio__title">E-commerce</h3>
+                    <a href="#portfolio-item-2" class="btn btn-primary">Más</a>
+                  </div>
+                </div>
+                    <div class="portfolio__item">
+                      <img src="http://imgfz.com/i/ojQLO6e.jpeg" alt="" class="portfolio__image"/>
+                      <div class="portfolio__desc">
+                        <h3 class="portfolio__title">E-commerce</h3>
+                        <a href="#portfolio-item-3" class="btn btn-primary">Más</a>
+                      </div>
+                    </div>
+                </div>
+
+                <div class="portfolio-lightbox" id="portfolio-item-1">
+                  <div class="portfolio-lightbox__content">
+                  <a href="#portfolio" class="close"></a> 
+                  <img class="portfolio-lightbox__image" src="http://imgfz.com/i/oVrt6KT.jpeg" alt=""/>
+                  <h3 class="portfolio__title">Blog</h3>
+                  <p class="portfolio__body">Sitio diseñado con Python y Flask en base de SQL.</p>
+                  </div>
+                </div> 
+
+                <div class="portfolio-lightbox" id="portfolio-item-2">
+                  <div class="portfolio-lightbox__content">
+                    <a href="#portfolio" class="close"></a>
+                    <img class="portfolio-lightbox__image" src="http://imgfz.com/i/o5IHu0b.jpeg" alt=""/>
+                    <h3 class="portfolio__title">E-commerce</h3>
+                    <p class="portfolio__body">Web diseñada en Front con Angular y Backend en Node - Express.</p>
+                  </div>
+                </div> 
+
+                <div class="portfolio-lightbox" id="portfolio-item-3">
+                  <div class="portfolio-lightbox__content">
+                  <a href="#portfolio" class="close"></a>
+                  <img class="portfolio-lightbox__image" src="http://imgfz.com/i/ojQLO6e.jpeg" alt=""/>
+                  <h3 class="portfolio__title">E-commerce</h3>
+                  <p class="portfolio__body">Web diseñada en Front con React y Backend en Node - Express.</p>
+                  </div>
+                </div> 
+          </div>
   )
     
 };
