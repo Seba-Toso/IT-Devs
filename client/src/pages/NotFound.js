8c97ae6580102384b932fc3d4598bdda865e6912
Seba Toso <jsgtoso@gmail.com>
2021-03-30 23:03:03 -0300

    Not Found section edited

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -5,11 +5,15 @@ const NotFound = () =>{
     return(
         <div className="NotFound">
             <div className='sloganContainer Error'>
-                  <h1 className='sloganText One'>Error</h1>
-                  <h1 className='sloganText Two'>404</h1>
-                  <h1 className='sloganText Three'>Sorry</h1>
+                  <h1 className='sloganText One'>ERROR 404</h1>
+                  <h1 className='sloganText Two'>PASARON</h1>
+                  <h1 className='sloganText Three'>COSAS...</h1>
             </div>
-            <img src="assets/404-error.png" alt='404 page error' />
+            <div className='section errorMessage'>
+                <h1>Uff... Algo no estuvo bueno y no salió como lo planeamos.</h1>
+                <p>Te recomendamos visitar nuestra sección de <i>PROYECTOS</i> para ver nuestros trabajos.</p>
+                <p>Si tienes un proyecto en mente, visita <i>PRESUPUESTA</i> para realizar una estimación del costo y contactarte con nosotros.</p>
+            </div>
         </div>
     )
 };
